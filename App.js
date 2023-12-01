2ffaabf8ede53315fe92d64ffdd05f28fba8cc70
Charlie <106312897+civilspeedy@users.noreply.github.com>
2023-12-01 19:33:18 +0000

    streak storage seems to be working

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { getStreak, setStreak, streak } from './logic';
+import Streak from './Streak';
 
 export default function App() {
-  setStreak("0");
-  getStreak()
-  console.log("streak var = ", streak);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Streak/>
     </View>
   );
 }
---
+++ Streak.js
@@ -0,0 +1,66 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+
+export default function Streak() {
+    const KEY = "@streak";
+    const [streak, setStreak] = useState('0');
+
+    const storeStreak = async (streak) => {
+        try {
+            await AsyncStorage.setItem(KEY, streak);
+            console.log("Streak has been saved as ", streak);
+        } 
+        catch (e) {
+            console.error("err in storeStreak ", e);
+        };
+    }
+
+    const resetStreak = async () => {
+        try {
+            await AsyncStorage.setItem(KEY, '0');
+            console.log("Streak has been reset")
+        }
+        catch (e) {
+            console.error("err in resetStreak ", e );
+        };
+    };
+
+    const readStreak = async () => {
+        try {
+            const streak = await AsyncStorage.getItem(KEY);
+            if (streak !== null) {
+                setStreak(streak);
+                console.log("reading Streak was succesfull ", streak);
+            }
+        }
+        catch (e) {
+            console.error("err in readStreak ", e);
+        };
+    };
+
+    const clearStorage = async () => {
+        try{
+            await AsyncStorage.clear();
+            console.log("AsyncStorage has been cleared");
+        }
+        catch (e) {
+            console.error("err in clearStreak() ", e);
+        }
+    };
+    
+    const updateStreak = () => {
+        const newStreak = (parseFloat(streak.substring()) + 1).toString();
+        storeStreak(newStreak);
+        console.log("new streak ", newStreak);
+        readStreak();
+    };
+
+    useEffect(() => {
+        readStreak();
+    }, []);
+
+    return (
+        <Text>{streak}</Text>
+    );
+};
