25ef3b2703728444dbc5f0ad31747802e282b9e1
charlie <charliejws03@gmail.com>
2023-12-28 21:23:18 +0000

    task settings need expanding

--- Components/TaskComponent.js
+++ Components/TaskComponent.js
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Pressable, LayoutAnimation } from "react-native";
+import { StyleSheet, View, Text, Pressable, LayoutAnimation, Alert } from "react-native";
 import { globalColours, smoothExpansionAnimation } from "../Styling/GlobalStyles";
 import { GestureHandlerRootView, LongPressGestureHandler, State } from "react-native-gesture-handler";
 import { impactAsync } from "expo-haptics";
 
 
 export default function TaskComponent({ task }) {
-    const [longPressed, isLongPressed] = useState(false);
+    const [isExtended, setExtended] = useState(false);
     // https://docs.swmansion.com/react-native-gesture-handler/docs/gestures/long-press-gesture
     const onLongPress = (event) => {
-        if (event.nativeEvent.state === State.BEGAN) {
+        if (event.nativeEvent.state === State.ACTIVE) {
             impactAsync();
+
         } else if (event.nativeEvent.state === State.END) {
-            console.log('long press!')
+            smoothExpansionAnimation();
+            setExtended(!isExtended);
+        } else if (event.nativeEvent.state === State.CANCELLED) {
+            Alert.alert("Long Press for menu")
         }
     };
 
-
-
-
     return (
-        <GestureHandlerRootView>
+        <View style={styles.container}>
             <LongPressGestureHandler
                 onGestureEvent={onLongPress}
                 onHandlerStateChange={onLongPress}>
@@ -29,19 +30,16 @@ export default function TaskComponent({ task }) {
                     <Text style={styles.taskStatText}>Due: {task.due}</Text>
                 </View>
             </LongPressGestureHandler>
-        </GestureHandlerRootView>
+            {isExtended && (
+                <Pressable>
+                    <Text>test</Text>
+                </Pressable>
+            )}
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: globalColours.backgroundSecondary,
-        padding: 10,
-        borderRadius: 20,
-        alignItems: 'flex-start',
-        marginHorizontal: 20,
-        marginVertical: 5
-    },
     taskTitle: {
         color: globalColours.secondary,
         fontWeight: 'bold',
@@ -58,6 +56,14 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         marginHorizontal: 20,
         padding: 5,
-
     },
+    pressableTextWrapper: {
+        flex: 1,
+        backgroundColor: globalColours.backgroundSecondary,
+        padding: 10,
+        borderRadius: 20,
+        alignItems: 'flex-start',
+        marginHorizontal: 20,
+        marginVertical: 5,
+    }
 })
