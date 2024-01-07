f922e3c3e60641eece3bc1f4426eec71a8ce480c
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-01-07 16:18:11 +0000

    emergency push

--- Components/EditModalComponent.js
+++ Components/EditModalComponent.js
@@ -10,161 +10,153 @@ import { ScrollView } from "react-native-gesture-handler";
 import { updateTask } from "../Logic/Database/DatabaseManipulation";
 
 export default function EditModal({ task, fetchTasks }) {
-    const [isOpen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-    const [title, setTitle] = useState(task.tile);
-    const [maxWords, setMaxWords] = useState(String(task.word_count));
-    const [due, setDue] = useState(task.due);
-    const [startDate, setStart] = useState(task.start_date);
-    const [maxPages, setPages] = useState(String(task.page_count));
-    const [slides, setSlides] = useState(String(task.slides_count));
-    const [subject, setSubject] = useState(task.subject);
+  const [title, setTitle] = useState(task.tile);
+  const [maxWords, setMaxWords] = useState(String(task.word_count));
+  const [due, setDue] = useState(task.due);
+  const [startDate, setStart] = useState(task.start_date);
+  const [maxPages, setPages] = useState(String(task.page_count));
+  const [slides, setSlides] = useState(String(task.slides_count));
+  const [subject, setSubject] = useState(task.subject);
 
+  const editBasedOnType = () => {
+    const type = task.format;
+    if (type == "Essay" || type == "Report" || type == "Project") {
+      return (
+        <View>
+          <CustomLabel text={"Edit Max Word Count:"} />
+          <NumberInput
+            value={maxWords}
+            setValue={setMaxWords}
+            placeholder={String(task.word_count)}
+          />
 
+          <CustomLabel text={"Edit Max Pages:"} />
+          <NumberInput value={maxPages} setValue={setPages} placeholder={String(task.page_count)} />
 
-    const editBasedOnType = () => {
-        const type = task.format;
-        if (type == 'Essay' || type == 'Report' || type == 'Project') {
-            return (
-                <View>
-                    <CustomLabel text={"Edit Max Word Count:"} />
-                    <NumberInput
-                        value={maxWords}
-                        setValue={setMaxWords}
-                        placeholder={String(task.word_count)} />
+          <CustomLabel text={"Edit Subject"} />
+          <CustomTextInput value={subject} setValue={setSubject} placeholder={task.subject} />
 
-                    <CustomLabel text={"Edit Max Pages:"} />
-                    <NumberInput
-                        value={maxPages}
-                        setValue={setPages}
-                        placeholder={String(task.page_count)} />
-
-                    <CustomLabel text={"Edit Subject"} />
-                    <CustomTextInput
-                        value={subject}
-                        setValue={setSubject}
-                        placeholder={task.subject} />
-
-                    <CustomLabel text={"Edit Due Date"} />
-                    <DateSlector
-                        setDate={setDue}
-                        date={due} />
-
-                </View>
-            );
-        }
-        if (type == 'Presentation') {
-            return (
-                <View>
-
-                    <CustomLabel text={"Edit Slide Count:"} />
-                    <NumberInput
-                        value={slides}
-                        setValue={setSlides}
-                        placeholder={String(task.slides_count)} />
-
-                    <CustomLabel text={"Edit Due Date"} />
-                    <DateSlector
-                        setDate={setDue}
-                        date={due} />
-
-                </View>
-            )
-        }
-    };
-
-    return (
-        <View style={styles.container}>
-            <Modal
-                animationType="slide"
-                visible={isOpen}
-                onRequestClose={() => setOpen(!isOpen)}
-                style={{ flex: 1 }}
-                transparent={true}>
-                <ScrollView style={styles.modal}>
-
-                    <CustomLabel text={"Edit Title:"} />
-                    <CustomTextInput
-                        setValue={setTitle}
-                        value={title}
-                        placeholder={task.title} />
-
-                    <CustomLabel text={"Edit Start Date:"} />
-                    <DateSlector
-                        date={startDate}
-                        setDate={setStart} />
-
-
-                    {editBasedOnType()}
-
-                    <View style={styles.bottomButtons}>
-                        <Pressable style={styles.cancel} onPress={() => setOpen(false)}>
-                            <Text>Cancel and Close</Text>
-                        </Pressable>
-
-                        <Pressable style={styles.confirmEdit} onPress={() => {
-                            updateTask({
-                                _pageCount: maxPages, _slideCount: slides, _wordCount: maxWords,
-                                _startDate: startDate, _dueDate: due, _subject: subject, done: false,
-                                _title: title
-                            }, task.title);
-                            fetchTasks();
-                            setOpen(false);
-                        }}>
-
-                            <Text>Confrim Changes</Text>
-                        </Pressable>
-                    </View>
-                </ScrollView>
-            </Modal>
-
-            <Pressable style={styles.editButton} onPress={() => setOpen(true)}>
-                <MaterialCommunityIcons name="lead-pencil" size={70} />
-            </Pressable>
+          <CustomLabel text={"Edit Due Date"} />
+          <DateSlector setDate={setDue} date={due} />
         </View>
-    )
-};
+      );
+    }
+    if (type == "Presentation") {
+      return (
+        <View>
+          <CustomLabel text={"Edit Slide Count:"} />
+          <NumberInput
+            value={slides}
+            setValue={setSlides}
+            placeholder={String(task.slides_count)}
+          />
+
+          <CustomLabel text={"Edit Due Date"} />
+          <DateSlector setDate={setDue} date={due} />
+        </View>
+      );
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType='slide'
+        visible={isOpen}
+        onRequestClose={() => setOpen(!isOpen)}
+        style={{ flex: 1 }}
+        transparent={true}
+      >
+        <ScrollView style={styles.modal}>
+          <CustomLabel text={"Edit Title:"} />
+          <CustomTextInput setValue={setTitle} value={title} placeholder={task.title} />
+
+          <CustomLabel text={"Edit Start Date:"} />
+          <DateSlector date={startDate} setDate={setStart} />
+
+          {editBasedOnType()}
+
+          <View style={styles.bottomButtons}>
+            <Pressable style={styles.cancel} onPress={() => setOpen(false)}>
+              <Text>Cancel and Close</Text>
+            </Pressable>
+
+            <Pressable
+              style={styles.confirmEdit}
+              onPress={() => {
+                updateTask(
+                  {
+                    _pageCount: maxPages,
+                    _slideCount: slides,
+                    _wordCount: maxWords,
+                    _startDate: startDate,
+                    _dueDate: due,
+                    _subject: subject,
+                    done: false,
+                    _title: title,
+                  },
+                  task.title
+                );
+                fetchTasks();
+                setOpen(false);
+              }}
+            >
+              <Text>Confrim Changes</Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </Modal>
+
+      <Pressable style={styles.editButton} onPress={() => setOpen(true)}>
+        <MaterialCommunityIcons name='lead-pencil' size={70} />
+      </Pressable>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    editButton: {
-        backgroundColor: 'orange',
-        flex: 1,
-        borderRadius: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginVertical: 10,
-    },
-    modal: {
-        backgroundColor: 'white',
-        flex: 1,
-        borderTopRightRadius: 20,
-        borderTopLeftRadius: 20,
-        padding: 10,
-    },
-    bottomButtons: {
-        flex: 1,
-        flexDirection: 'row',
-    },
-    confirmEdit: {
-        flex: 1,
-        backgroundColor: globalColours.tertiary,
-        borderRadius: 20,
-        padding: 20,
-        margin: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    cancel: {
-        flex: 1,
-        backgroundColor: 'red',
-        borderRadius: 20,
-        padding: 20,
-        margin: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-
+  container: {
+    flex: 1,
+  },
+  editButton: {
+    backgroundColor: "orange",
+    flex: 1,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+    width: 70,
+    height: 70,
+  },
+  modal: {
+    backgroundColor: "white",
+    flex: 1,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    padding: 10,
+  },
+  bottomButtons: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  confirmEdit: {
+    flex: 1,
+    backgroundColor: globalColours.tertiary,
+    borderRadius: 20,
+    padding: 20,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cancel: {
+    flex: 1,
+    backgroundColor: "red",
+    borderRadius: 20,
+    padding: 20,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- Components/TaskComponent.js
+++ Components/TaskComponent.js
@@ -1,106 +1,143 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Pressable, LayoutAnimation, Alert } from "react-native";
+import { StyleSheet, View, Pressable, Alert, Text } from "react-native";
 import { globalColours, smoothExpansionAnimation } from "../Styling/GlobalStyles";
 import { impactAsync } from "expo-haptics";
 import { GestureDetector, Gesture } from "react-native-gesture-handler";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { deleteTask, setTaskDone } from "../Logic/Database/DatabaseManipulation";
 import EditModal from "./EditModalComponent";
-import MoreButton from "./Output Components/MoreButtonComponent";
-
 
 export default function TaskComponent({ task, fetchTasks }) {
-    const [isExtended, setExtended] = useState(false);
-    // https://docs.swmansion.com/react-native-gesture-handler/docs/gestures/long-press-gesture
+  const [isExtended, setExtended] = useState(false);
+  // https://docs.swmansion.com/react-native-gesture-handler/docs/gestures/long-press-gesture
 
-    const whenLongPress = Gesture.LongPress().onEnd((e, success) => {
-        smoothExpansionAnimation();
-        impactAsync();
-        setExtended(!isExtended)
-    });
+  const whenLongPress = Gesture.LongPress().onEnd((e, success) => {
+    smoothExpansionAnimation();
+    impactAsync();
+    setExtended(!isExtended);
+  });
 
-    const promptDeleteTask = () => {
-        Alert.alert('Delete Task?',
-            'Are you sure you want to delete this Task?',
-            [{ text: 'No', },
-            {
-                text: 'yes', onPress: () => {
-                    deleteTask(task.title);
-                    fetchTasks();
-                }
-            }
-            ])
-    };
+  const promptDeleteTask = () => {
+    Alert.alert("Delete Task?", "Are you sure you want to delete this Task?", [
+      { text: "No" },
+      {
+        text: "yes",
+        onPress: () => {
+          deleteTask(task.title);
+          fetchTasks();
+        },
+      },
+    ]);
+  };
 
-    const doneTask = () => {
-        setTaskDone(task, true);
-        fetchTasks();
-    };
+  const doneTask = () => {
+    setTaskDone(task, true);
+    fetchTasks();
+  };
 
-    return (
-        <View style={styles.container}>
-            <GestureDetector
-                gesture={whenLongPress}
-                shouldCancelWhenOutside={true}
-                onPressOut={() => impactAsync()}>
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                    
-                    {!isExtended && (
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
+  return (
+    <View style={styles.container}>
+      <GestureDetector
+        gesture={whenLongPress}
+        shouldCancelWhenOutside={true}
+        onPressOut={() => impactAsync()}
+      >
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          {!isExtended && (
+            <View style={{ flex: 1, flexDirection: "row" }}>
+              <View style={styles.textWrapper}>
+                <Text style={styles.taskTitle}>{task.title}</Text>
+                <Text style={styles.taskStatText}>{task.due}</Text>
+              </View>
 
-                            <MoreButton task={task} />
+              <Pressable style={styles.doneButton} onPress={doneTask}>
+                <MaterialCommunityIcons name='check' size={70} />
+              </Pressable>
+            </View>
+          )}
+        </View>
+      </GestureDetector>
+      {isExtended && (
+        <View style={{ flex: 1 }}>
+          <EditModal task={task} fetchTasks={fetchTasks} />
 
-                            <Pressable style={styles.doneButton} onPress={doneTask}>
-                                <MaterialCommunityIcons
-                                    name='check'
-                                    size={70} />
-                            </Pressable>
-                        </View>
-                    )}
-
-                </View>
-            </GestureDetector >
-            {isExtended && (
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                    <EditModal task={task} fetchTasks={fetchTasks} />
-
-                    <Pressable style={styles.deleteButton} onPress={promptDeleteTask}>
-                        <MaterialCommunityIcons
-                            name="delete-outline"
-                            size={70}
-                            style={{ alignSelf: 'center' }} />
-                    </Pressable>
-                </View>
-            )
-            }
-        </View >
-    );
-};
+          <Pressable style={styles.deleteButton} onPress={promptDeleteTask}>
+            <MaterialCommunityIcons
+              name='delete-outline'
+              size={70}
+              style={{ alignSelf: "center" }}
+            />
+          </Pressable>
+        </View>
+      )}
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: globalColours.backgroundSecondary,
-        flex: 1,
-        marginHorizontal: 20,
-        marginVertical: 5,
-        flexDirection: 'row',
-        borderRadius: 20,
-    },
-    deleteButton: {
-        backgroundColor: 'red',
-        flex: 1,
-        borderRadius: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 10,
-    },
-    doneButton: {
-        backgroundColor: globalColours.tertiary,
-        borderRadius: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 10,
-        width: 70,
-        height: 70,
-    },
-})
+  container: {
+    backgroundColor: globalColours.backgroundSecondary,
+    flex: 1,
+    marginHorizontal: 20,
+    marginVertical: 5,
+    flexDirection: "row",
+    borderRadius: 20,
+  },
+  deleteButton: {
+    backgroundColor: "red",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    margin: 10,
+    width: 70,
+    height: 70,
+  },
+  doneButton: {
+    backgroundColor: globalColours.tertiary,
+    borderRadius: 20,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    margin: 10,
+    width: 70,
+    height: 70,
+  },
+  taskTitle: {
+    color: globalColours.secondary,
+    fontWeight: "bold",
+    fontSize: 30,
+    alignSelf: "center",
+  },
+  taskStatText: {
+    fontSize: 15,
+    alignSelf: "center",
+  },
+  textWrapper: {
+    backgroundColor: globalColours.backgroundSecondary,
+    padding: 10,
+    flex: 1,
+    borderRadius: 20,
+    alignItems: "flex-start",
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  titleContainer: {
+    alignSelf: "center",
+    backgroundColor: globalColours.tertiary,
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  formatContainer: {
+    alignSelf: "center",
+    backgroundColor: globalColours.tertiary,
+    alignItems: "center",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+});
